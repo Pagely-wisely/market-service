5e6ca8e714daaf4d9c13b99c3f886c5a858a5a32
soo <dlgustn96@gmail.com>
2026-04-24 02:12:15 +0900

    chore: initialize project repository

---
+++ src/test/java/com/pagely/marketservice/MarketserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagely.marketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
