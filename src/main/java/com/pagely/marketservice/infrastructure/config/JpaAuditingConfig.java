66079171dcaf2d5ceed46ee46fc69bb10ab2c783
soo <dlgustn96@gmail.com>
2026-04-27 16:10:12 +0900

    feat: JpaAuditingConfig 설정

---
+++ src/main/java/com/pagely/marketservice/infrastructure/config/JpaAuditingConfig.java
@@ -0,0 +1,19 @@
+package com.pagely.marketservice.infrastructure.config;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<UUID> auditorAware() {
+        // TODO: 임시로 구현
+        return () -> Optional.of(UUID.fromString("00000000-0000-0000-0000-000000000000"));
+    }
+}
