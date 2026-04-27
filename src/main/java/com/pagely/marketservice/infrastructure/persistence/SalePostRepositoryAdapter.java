146b50b02e6cb874e1c49a1b249e3d2218320828
soo <dlgustn96@gmail.com>
2026-04-27 20:18:59 +0900

    feat: 판매글 상세 조회 API 구현
    Related to: #4
    Resolves: #7

--- src/main/java/com/pagely/marketservice/infrastructure/persistence/SalePostRepositoryAdapter.java
+++ src/main/java/com/pagely/marketservice/infrastructure/persistence/SalePostRepositoryAdapter.java
@@ -2,6 +2,8 @@ package com.pagely.marketservice.infrastructure.persistence;
 
 import com.pagely.marketservice.domain.model.SalePost;
 import com.pagely.marketservice.domain.repository.SalePostRepository;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +16,10 @@ public class SalePostRepositoryAdapter implements SalePostRepository {
     @Override
     public SalePost save(SalePost salePost) {
         return jpaSalePostRepository.save(salePost);
+    }
+
+    @Override
+    public Optional<SalePost> findById(UUID salePostId) {
+        return jpaSalePostRepository.findById(salePostId);
     }
 }
